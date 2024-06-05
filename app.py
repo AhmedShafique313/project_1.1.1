4599c789c1bd7b7da18cb78d3d176e0e6df54e17
AhmedShafique313 <2021ee313@student.uet.edu.pk>
2024-06-05 04:51:33 -0700

    templates and static folder added in project

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
-def hello():
-    return "Hello_World"
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-   app.run()
+   app.run(debug=True)
+# for getting less error lets make app.run(debug=True)
